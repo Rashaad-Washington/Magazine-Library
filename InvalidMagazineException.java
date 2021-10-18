90f9700950d6ac6904814997dec65708dcc1915e
Rashaad H. Washington <86685926+Rashaad-Washington@users.noreply.github.com>
2021-10-17 20:04:21 -0500

    Add files via upload

---
+++ InvalidMagazineException.java
@@ -0,0 +1,14 @@
+package linkedlist.exception;
+
+/**
+ * @author Rashaad Washington
+ * CSCI 3250
+ * Assignment 2
+ */
+public class InvalidMagazineException extends Exception{
+
+    public InvalidMagazineException(String message) {
+        super(message);
+    }
+    
+}
---
+++ ListException.java
@@ -0,0 +1,13 @@
+
+package linkedlist.exception;
+
+/**
+ * @author Rashaad Washington
+ * CSCI 3250
+ * Assignment 2
+ */
+public class ListException extends Exception {
+    public ListException(String message){
+        super(message);
+    }
+}
