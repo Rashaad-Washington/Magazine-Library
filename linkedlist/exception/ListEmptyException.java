29d2871420ab11e985c39edf0e0dc81201cfe89c
Rashaad H. Washington <86685926+Rashaad-Washington@users.noreply.github.com>
2021-10-17 20:08:23 -0500

    Add files via upload

---
+++ linkedlist/exception/ListEmptyException.java
@@ -0,0 +1,13 @@
+package linkedlist.exception;
+
+/**
+ * @author Rashaad Washington
+ * CSCI 3250
+ * Assignment 2
+ */
+public class ListEmptyException extends Exception {
+
+    public ListEmptyException(String message) {
+        super(message);
+    }
+}
